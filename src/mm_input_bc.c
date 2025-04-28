5c54bc38f225f2f0475a724921f499b0e1de416e
Weston Ortiz <weston@wortiz.com>
2025-04-28 09:25:09 -0600

    basic 3D elliptic

--- src/mm_input_bc.c
+++ src/mm_input_bc.c
@@ -422,9 +422,11 @@ void rd_bc_specs(FILE *ifp, char *input) {
     case CURRENT_SIC_BC:
     case ELLIPTIC_XI_REGULARIZATION_BC:
     case ELLIPTIC_ETA_REGULARIZATION_BC:
+    case ELLIPTIC_ZETA_REGULARIZATION_BC:
     case KINEMATIC_BC:
     case KINEMATIC_XI_BC:
     case KINEMATIC_ETA_BC:
+    case KINEMATIC_ZETA_BC:
     case LUB_KINEMATIC_BC:
     case KIN_LEAK_HEAT_BC:
     case KINEMATIC_PETROV_BC:
