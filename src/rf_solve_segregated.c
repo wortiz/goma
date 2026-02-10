ce9ae9c0c09cfe4f5e6b04c7da4cf0445a91ec7c
Weston Ortiz <weston@wortiz.com>
2026-02-10 12:51:36 -0700

    tmp

--- src/rf_solve_segregated.c
+++ src/rf_solve_segregated.c
@@ -1224,6 +1224,7 @@ void solve_problem_segregated(Exo_DB *exo, /* ptr to the finite element mesh dat
             case HUYGENS_C:
             case HUYGENS_MASS_ITER:
             case FACET_BASED:
+            case FACET_BASED_SMOLIANSKI:
               Renorm_Now =
                   (ls->Force_Initial_Renorm || (ls->Renorm_Freq != 0 && ls->Renorm_Countdown == 0));
 
@@ -2442,6 +2443,7 @@ void solve_problem_segregated(Exo_DB *exo, /* ptr to the finite element mesh dat
           case HUYGENS_C:
           case HUYGENS_MASS_ITER:
           case FACET_BASED:
+          case FACET_BASED_SMOLIANSKI:
             Renorm_Now =
                 (ls->Renorm_Freq != 0 && ls->Renorm_Countdown == 0) || ls_adc_event == TRUE;
 
